0ff089da4d4ffd06ad6aa14e32bbe6613661ed3a
PopCPS <arturcoelho.cps@gmail.com>
2024-09-12 17:20:41 -0300

    update/add user feedback on requests

--- src/pages/components/modal.tsx
+++ src/pages/components/modal.tsx
@@ -1,7 +1,6 @@
 import { X } from "lucide-react";
 import { ReactNode } from "react";
 import { tv, VariantProps } from 'tailwind-variants'
-import { Loading } from "./loading";
 
 const modalVariants = tv({
   base: 'rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5',
